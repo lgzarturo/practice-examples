52ab2ea6798a0aa4ff4f8b455e2d97806c7ed27d
lgzarturo <lgzarturo@gmail.com>
2024-06-28 13:12:42 -0500

    feat: leer un valor de la consola

--- src/Example01.java
+++ src/Example01.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Example01 {
     public static void main(String[] args) {
-        System.out.println(Greetings.greet("World"));
+        System.out.println("What's your name?");
+        Scanner scanner = new Scanner(System.in);
+        var name = scanner.nextLine();
+        if (name == null || name.isBlank()) {
+            System.err.println("No name provided");
+            System.exit(1);
+        }
+        System.out.println(Greetings.greet(name));
     }
 }
