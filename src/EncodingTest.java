351fc9d43e9455e225ed3ddfaa1398a706cad69f
lgzarturo <lgzarturo@gmail.com>
2024-07-04 14:03:41 -0500

    feat: pruebas para entender la codificación UTF8
    quería validar si hay una forma de corregir la codificación de un archivo.

--- src/EncodingTest.java
+++ src/EncodingTest.java
@@ -1,8 +1,12 @@
 import java.nio.file.Path;
+import java.util.logging.Logger;
 
 public class EncodingTest {
+    private static final Logger logger = Logger.getLogger(EncodingTest.class.getName());
+
     public static void main(String[] args) {
-        String directoryPath = "/Users/lgzarturo/IdeaProjects/metal/core-api/src/";
+        String directoryPath = "/Users/lgzarturo/temp/metal";
+        logger.info("Checking encoding of files in directory: " + directoryPath);
         EncodingValidator.isFilesInDirectoryEncodingUTF8(Path.of(directoryPath));
     }
 }
--- src/EncodingValidator.java
+++ src/EncodingValidator.java
@@ -2,6 +2,7 @@ import org.mozilla.universalchardet.UniversalDetector;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.logging.Level;
@@ -20,6 +21,11 @@ public class EncodingValidator {
                 .forEach(file -> {
                     boolean isUTF8 = isFileEncodingUTF8(file);
                     logger.info(format("-- Is file %s encoding UTF-8? %s%n", file, isUTF8));
+                    if (!isUTF8) {
+                        correctEncoding(file);
+                    }
+                    getCreatedAndUpdatedFile(file);
+                    getPermissions(file);
                 });
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Error while processing files in directory", e);
@@ -44,6 +50,51 @@ public class EncodingValidator {
         } catch (Exception e) {
             logger.log(Level.SEVERE, "Error while processing file", e);
             return false;
+        }
+    }
+
+    public static void correctEncoding(Path filePath) {
+        try {
+            String content = Files.readString(filePath);
+            Files.writeString(filePath, content, StandardCharsets.UTF_8);
+            logger.info(format("Corrected encoding for file: %s", filePath));
+            var isUTF8 = isFileEncodingUTF8(filePath);
+            if (!isUTF8) {
+                logger.warning(format("Could not correct encoding for file: %s", filePath));
+            } else {
+                logger.info(format("File %s encoding is now UTF-8", filePath));
+            }
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error while correcting file encoding", e);
+        }
+    }
+
+    public static void getCreatedAndUpdatedFile(Path filePath) {
+        try {
+            var createdTime = Files.getAttribute(filePath, "creationTime");
+            var updatedTime = Files.getAttribute(filePath, "lastModifiedTime");
+            var created = getHumanReadableDate(createdTime.toString());
+            var updated = getHumanReadableDate(updatedTime.toString());
+            logger.info(format("-- File %s was created at %s and last updated at %s", filePath, created, updated));
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error while getting file creation and update time", e);
+        }
+    }
+
+
+    public static String getHumanReadableDate(String date) {
+        String dateHumanReadable = date.replace("T", " ").replace("Z", "");
+        String time = dateHumanReadable.split(" ")[1];
+        String timeHumanReadable = time.split(":")[0] + ":" + time.split(":")[1];
+        return String.format("%s %s", dateHumanReadable.split(" ")[0], timeHumanReadable);
+    }
+
+    public static void getPermissions(Path filePath) {
+        try {
+            var permissions = Files.getPosixFilePermissions(filePath);
+            logger.info(format("-- File %s has permissions: %s", filePath, permissions));
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error while getting file permissions", e);
         }
     }
 }
